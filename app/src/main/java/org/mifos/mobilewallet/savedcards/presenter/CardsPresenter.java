66aad4dd718bfbe018b31eca66e2a57392cb2f38
Ankur Sharma <ankurs287@gmail.com>
2018-05-22 18:59:03 +0530

    feat: Saved Credit Card UI and Backend

--- app/src/main/java/org/mifos/mobilewallet/savedcards/presenter/CardsPresenter.java
+++ app/src/main/java/org/mifos/mobilewallet/savedcards/presenter/CardsPresenter.java
@@ -141,4 +141,28 @@ public class CardsPresenter implements CardsContract.CardsPresenter {
                     }
                 });
     }
+
+    private boolean validateCreditCardNumber(String str) {
+
+        int[] ints = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            ints[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        for (int i = ints.length - 2; i >= 0; i = i - 2) {
+            int j = ints[i];
+            j = j * 2;
+            if (j > 9) {
+                j = j % 10 + 1;
+            }
+            ints[i] = j;
+        }
+        int sum = 0;
+        for (int i = 0; i < ints.length; i++) {
+            sum += ints[i];
+        }
+        if (sum % 10 == 0) {
+            return true;
+        }
+        return false;
+    }
 }
