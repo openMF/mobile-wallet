bda483c18bbce2e44b3e2b9c478a612341fc7deb
letelete <brunokawka@gmail.com>
2018-12-08 18:38:57 +0100

    Add connection: edit profile, fetch data

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/home/ui/ProfileFragment.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/home/ui/ProfileFragment.java
@@ -1,5 +1,6 @@
 package org.mifos.mobilewallet.mifospay.home.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,6 +15,7 @@ import org.mifos.mobilewallet.core.domain.model.client.Client;
 import org.mifos.mobilewallet.mifospay.R;
 import org.mifos.mobilewallet.mifospay.base.BaseActivity;
 import org.mifos.mobilewallet.mifospay.base.BaseFragment;
+import org.mifos.mobilewallet.mifospay.editprofile.ui.EditProfileActivity;
 import org.mifos.mobilewallet.mifospay.home.BaseHomeContract;
 import org.mifos.mobilewallet.mifospay.home.presenter.ProfilePresenter;
 import org.mifos.mobilewallet.mifospay.utils.Constants;
@@ -82,11 +84,15 @@ public class ProfileFragment extends BaseFragment implements BaseHomeContract.Pr
 
         setupUi();
 
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
         mProfilePresenter.fetchProfile();
         mProfilePresenter.fetchAccountDetails();
         mProfilePresenter.fetchClientImage();
-
-        return rootView;
     }
 
     private void setupUi() {
@@ -123,6 +129,9 @@ public class ProfileFragment extends BaseFragment implements BaseHomeContract.Pr
 
     @OnClick(R.id.btn_profile_bottom_sheet_action)
     public void onEditProfileClicked() {
+        if (getActivity() != null) {
+            getActivity().startActivity(new Intent(getActivity(), EditProfileActivity.class));
+        }
     }
 
     @Override
