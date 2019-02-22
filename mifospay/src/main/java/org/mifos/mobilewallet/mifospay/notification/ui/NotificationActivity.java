128ebf57bba50526384d3438eb357747cd1715f7
jasnasuresh <jasna.suresh@gmail.com>
2019-02-22 23:17:51 +0530

    added a message in  notifications activity

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/notification/ui/NotificationActivity.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/notification/ui/NotificationActivity.java
@@ -5,12 +5,15 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import org.mifos.mobilewallet.core.domain.model.NotificationPayload;
 import org.mifos.mobilewallet.mifospay.R;
 import org.mifos.mobilewallet.mifospay.base.BaseActivity;
 import org.mifos.mobilewallet.mifospay.notification.NotificationContract;
 import org.mifos.mobilewallet.mifospay.notification.presenter.NotificationPresenter;
+import org.mifos.mobilewallet.mifospay.utils.DebugUtil;
 import org.mifos.mobilewallet.mifospay.utils.Toaster;
 
 import java.util.List;
@@ -33,8 +36,10 @@ public class NotificationActivity extends BaseActivity implements
     NotificationPresenter mPresenter;
     NotificationContract.NotificationPresenter mNotificationPresenter;
 
-    @BindView(R.id.rv_notification)
+    @BindView(R.id.rv_notifications)
     RecyclerView mRvNotification;
+    @BindView(R.id.tv_placeholder)
+    TextView tvplaceholder;
 
     @Inject
     NotificationAdapter mNotificationAdapter;
@@ -81,6 +86,16 @@ public class NotificationActivity extends BaseActivity implements
 
     @Override
     public void fetchNotificationsSuccess(List<NotificationPayload> notificationPayloadList) {
+        if (notificationPayloadList == null || notificationPayloadList.size() == 0) {
+            DebugUtil.log("null");
+            mRvNotification.setVisibility(View.GONE);
+            tvplaceholder.setVisibility(View.VISIBLE);
+        } else {
+            DebugUtil.log("yes");
+            mRvNotification.setVisibility(View.VISIBLE);
+            tvplaceholder.setVisibility(View.GONE);
+            mNotificationAdapter.setNotificationPayloadList(notificationPayloadList);
+        }
         hideSwipeProgress();
         mNotificationAdapter.setNotificationPayloadList(notificationPayloadList);
     }
