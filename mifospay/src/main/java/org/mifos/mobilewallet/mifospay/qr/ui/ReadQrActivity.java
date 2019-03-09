b38861d1158ad547be6fb7e8dacbe8b318ffba99
vipinsoni38@gmail.com <vipinsoni38@gmail.com>
2019-03-09 13:51:55 +0530

    Added Scan Qr from Gallery

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/qr/ui/ReadQrActivity.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/qr/ui/ReadQrActivity.java
@@ -2,11 +2,30 @@ package org.mifos.mobilewallet.mifospay.qr.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import android.widget.Button;
+import android.widget.Toast;
+import butterknife.OnClick;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.FormatException;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Reader;
 import com.google.zxing.Result;
 
+import com.google.zxing.common.HybridBinarizer;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Hashtable;
 import org.mifos.mobilewallet.mifospay.R;
 import org.mifos.mobilewallet.mifospay.base.BaseActivity;
 import org.mifos.mobilewallet.mifospay.qr.QrContract;
@@ -26,6 +45,7 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 public class ReadQrActivity extends BaseActivity implements QrContract.ReadQrView,
         ZXingScannerView.ResultHandler {
 
+    private static final int SELECT_PHOTO = 100;
     @Inject
     ReadQrPresenter mPresenter;
 
@@ -33,6 +53,9 @@ public class ReadQrActivity extends BaseActivity implements QrContract.ReadQrVie
 
     @BindView(R.id.scannerView)
     ZXingScannerView mScannerView;
+
+    @BindView(R.id.from_gallery)
+    Button imageFromGallery;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -63,6 +86,60 @@ public class ReadQrActivity extends BaseActivity implements QrContract.ReadQrVie
         mScannerView.stopCamera();
     }
 
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            InputStream imageStream = null;
+            try {
+                imageStream = getContentResolver().openInputStream(selectedImage);
+            } catch (FileNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "File not found", Toast.LENGTH_SHORT)
+                        .show();
+            }
+            Bitmap bMap = BitmapFactory.decodeStream(imageStream);
+            int[] intArray = new int[bMap.getWidth() * bMap.getHeight()];
+            bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(),
+                    bMap.getHeight());
+            LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(),
+                    bMap.getHeight(), intArray);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+            Reader reader = new MultiFormatReader();
+            try {
+                Hashtable<DecodeHintType, Object> decodeHints =
+                        new Hashtable<>();
+                decodeHints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+                decodeHints.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+
+                Result result = reader.decode(bitmap, decodeHints);
+                String barcode =  result.getText().toString();
+
+                if (barcode != null) {
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra(Constants.QR_DATA, barcode);
+                    setResult(Activity.RESULT_OK, returnIntent);
+                    finish();
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error !", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            } catch (NotFoundException e) {
+                Toast.makeText(getApplicationContext(), "Error !", Toast.LENGTH_SHORT).show();
+            } catch (ChecksumException e) {
+                Toast.makeText(getApplicationContext(), "Error !", Toast.LENGTH_SHORT).show();
+            } catch (FormatException e) {
+                Toast.makeText(getApplicationContext(), "Error !", Toast.LENGTH_SHORT).show();
+            } catch (NullPointerException e) {
+                Toast.makeText(getApplicationContext(), "Error !", Toast.LENGTH_SHORT).show();
+            }
+
+
+
+        }
+    }
+
     @Override
     public void handleResult(Result result) {
         String qrData = result.getText();
@@ -76,5 +153,14 @@ public class ReadQrActivity extends BaseActivity implements QrContract.ReadQrVie
     @Override
     public void setPresenter(QrContract.ReadQrPresenter presenter) {
         this.mReadQrPresenter = presenter;
+    }
+
+    @OnClick(R.id.from_gallery)
+    public void openGallery() {
+
+        Intent photoPic = new Intent(Intent.ACTION_PICK);
+        photoPic.setType("image/*");
+        startActivityForResult(photoPic, SELECT_PHOTO);
+
     }
 }
