1091f7042f7cb9fe97c0aa35359bafe53188738e
Naman Dwivedi <namandwivedi14@gmail.com>
2019-04-02 13:03:28 +0530

    payment hub entities

---
+++ core/src/main/java/org/mifos/mobilewallet/core/data/paymenthub/api/BaseURL.java
@@ -0,0 +1,19 @@
+package org.mifos.mobilewallet.core.data.paymenthub.api;
+
+/**
+ * Created by naman on 17/6/17.
+ */
+
+public class BaseURL {
+
+    public static final String PROTOCOL_HTTPS = "https://";
+
+    public static final String API_ENDPOINT = "payments.dpc.hu";
+    public static final String API_PATH = "/in01/channel/transactions/";
+
+
+    public String getUrl() {
+        return PROTOCOL_HTTPS + API_ENDPOINT + API_PATH;
+
+    }
+}
