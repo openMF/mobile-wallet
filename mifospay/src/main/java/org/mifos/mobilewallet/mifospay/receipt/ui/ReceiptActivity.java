20bb07b496567642888c09bdfcd6733b00c08f1d
Shivansh <shivansh16tiwari@gmail.com>
2019-06-05 22:59:26 +0530

    feat: redesign receipt view and download

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/receipt/ui/ReceiptActivity.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/receipt/ui/ReceiptActivity.java
@@ -5,6 +5,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,8 +16,9 @@ import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
 
-import com.github.barteksc.pdfviewer.PDFView;
 
+import org.mifos.mobilewallet.core.data.fineract.entity.accounts.savings.TransferDetail;
+import org.mifos.mobilewallet.core.domain.model.Transaction;
 import org.mifos.mobilewallet.mifospay.R;
 import org.mifos.mobilewallet.mifospay.base.BaseActivity;
 import org.mifos.mobilewallet.mifospay.receipt.ReceiptContract;
@@ -32,6 +34,7 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import okhttp3.ResponseBody;
 
 public class ReceiptActivity extends BaseActivity implements ReceiptContract.ReceiptView {
@@ -43,13 +46,30 @@ public class ReceiptActivity extends BaseActivity implements ReceiptContract.Rec
 
     ReceiptContract.ReceiptPresenter mReceiptPresenter;
 
-    @BindView(R.id.pdfView_receipt)
-    PDFView pdfViewReceipt;
 
-    @BindView(R.id.tv_receiptLink)
+    @BindView(R.id.tv_amount)
+    TextView tvAmount;
+    @BindView(R.id.tv_operation)
+    TextView tvOperation;
+    @BindView(R.id.tv_name)
+    TextView tvPaidToName;
+    @BindView(R.id.tv_transaction_ID)
+    TextView tvTransactionID;
+    @BindView(R.id.tv_transaction_date)
+    TextView tvDate;
+    @BindView(R.id.tv_transaction_to_name)
+    TextView tvTransToName;
+    @BindView(R.id.tv_transaction_to_number)
+    TextView tvTransToNumber;
+    @BindView(R.id.tv_transaction_from_name)
+    TextView tvTransFromName;
+    @BindView(R.id.tv_transaction_from_number)
+    TextView tvTransFromNumber;
+    @BindView(R.id.tv_transaction_reciept)
     TextView tvReceiptLink;
 
     private String transactionId;
+    private boolean isDebit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,23 +104,70 @@ public class ReceiptActivity extends BaseActivity implements ReceiptContract.Rec
                         return true;
                     }
                 });
-
-                if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-                    // Permission is not granted
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            REQUEST_WRITE_EXTERNAL_STORAGE);
-                } else {
-                    // Permission already granted
-                    showProgressDialog(Constants.PLEASE_WAIT);
-                    mPresenter.fetchReceipt(transactionId);
-                }
             } catch(IndexOutOfBoundsException e) {
                 showToast("Invalid link used to open the App.");
             }
+            showProgressDialog(Constants.PLEASE_WAIT);
+            mPresenter.fetchTransaction(Long.parseLong(transactionId));
+        }
+    }
+
+    @Override
+    public void showTransactionDetail(Transaction transaction) {
+        tvAmount.setText(transaction.getCurrency().getCode() + " " + transaction.getAmount());
+        tvDate.setText(transaction.getDate());
+        tvReceiptLink.setText(Constants.RECEIPT_DOMAIN + transaction.getTransactionId());
+        tvTransactionID.setText(String.valueOf(transaction.getTransactionId()));
+
+
+        switch (transaction.getTransactionType()) {
+            case DEBIT:
+                isDebit = true;
+                tvOperation.setText("Paid to");
+                tvOperation.setTextColor(Color.RED);
+                break;
+            case CREDIT:
+                isDebit = false;
+                tvOperation.setText("Credited By");
+                tvOperation.setTextColor(Color.parseColor("#009688"));
+                break;
+            case OTHER:
+                isDebit = false;
+                tvOperation.setText(Constants.OTHER);
+                tvOperation.setTextColor(Color.YELLOW);
+                break;
+        }
+
+    }
+
+    @Override
+    public void showTransferDetail(TransferDetail transferDetail) {
+        if (isDebit) {
+            tvPaidToName.setText(transferDetail.getToClient().getDisplayName());
+        } else {
+            tvPaidToName.setText(transferDetail.getFromClient().getDisplayName());
+        }
+        tvTransToName.setText(Constants.NAME + transferDetail.getToClient().getDisplayName());
+        tvTransToNumber.setText(Constants.ACCOUNT_NUMBER + transferDetail.getToAccount().getAccountNo());
+        tvTransFromName.setText(Constants.NAME + transferDetail.getFromClient().getDisplayName());
+        tvTransFromNumber.setText(Constants.ACCOUNT_NUMBER + transferDetail.getFromAccount().getAccountNo());
+        hideProgressDialog();
+    }
+
+    @OnClick(R.id.fab_download)
+    void initiateDownload() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_EXTERNAL_STORAGE);
+        } else {
+            // Permission already granted
+            showSnackbar("Downloading Receipt");
+            mPresenter.downloadReceipt(transactionId);
         }
     }
 
@@ -119,48 +186,38 @@ public class ReceiptActivity extends BaseActivity implements ReceiptContract.Rec
     }
 
     @Override
-    public void writeReceipt(ResponseBody responseBody, String filename) {
+    public void writeReceiptToPDF(ResponseBody responseBody, String filename) {
 
         File mifosDirectory = new File(Environment.getExternalStorageDirectory(),
                 Constants.MIFOSPAY);
         if (!mifosDirectory.exists()) {
             mifosDirectory.mkdirs();
         }
-
         File documentFile = new File(mifosDirectory.getPath(), filename);
         if (!FileUtils.writeInputStreamDataToFile(responseBody.byteStream(), documentFile)) {
-            hideProgressDialog();
             showToast(Constants.ERROR_DOWNLOADING_RECEIPT);
         } else {
-            pdfViewReceipt.fromFile(documentFile).load();
-            hideProgressDialog();
             showSnackbar(Constants.RECEIPT_DOWNLOADED_SUCCESSFULLY);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-            @NonNull int[] grantResults) {
+                                           @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_WRITE_EXTERNAL_STORAGE: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // storage-related task you need to do.
 
-                    showProgressDialog(Constants.PLEASE_WAIT);
-                    mReceiptPresenter.fetchReceipt(transactionId);
+                    showSnackbar("Downloading Receipt");
+                    mReceiptPresenter.downloadReceipt(transactionId);
 
                 } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
                     showToast(Constants.NEED_EXTERNAL_STORAGE_PERMISSION_TO_DOWNLOAD_RECEIPT);
                 }
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request.
         }
     }
 
