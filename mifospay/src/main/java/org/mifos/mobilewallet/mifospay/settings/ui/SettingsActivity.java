d95c2c4946a25999f02b0c2fb310f25b6d787101
Rachit Gupta <44428198+rachittt@users.noreply.github.com>
2019-12-09 17:28:53 +0530

    feat: dialog box for logout button (#580)
    updated readme with new instructions
    
    changes done

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/settings/ui/SettingsActivity.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/settings/ui/SettingsActivity.java
@@ -1,5 +1,7 @@
 package org.mifos.mobilewallet.mifospay.settings.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -39,8 +41,18 @@ public class SettingsActivity extends BaseActivity implements SettingsContract.S
 
     @OnClick(R.id.btn_logout)
     public void onLogoutClicked() {
-        showProgressDialog(Constants.LOGGING_OUT);
-        mSettingsPresenter.logout();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppTheme_Dialog);
+        builder.setTitle(R.string.log_out_title);
+        builder.setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        showProgressDialog(Constants.LOGGING_OUT);
+                        mPresenter.logout();
+                    }
+                })
+                .setNegativeButton("No", null);
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     @OnClick(R.id.btn_disable_account)
