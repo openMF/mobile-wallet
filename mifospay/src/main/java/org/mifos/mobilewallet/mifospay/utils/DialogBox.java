3121905d649fb39f5a9bfb3ba8f6e84dcf4afbfd
Laxya Pahuja <laxya.pahuja8@gmail.com>
2019-12-23 22:43:12 +0530

    feat: common function for all dialog boxes

---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/utils/DialogBox.java
@@ -0,0 +1,47 @@
+package org.mifos.mobilewallet.mifospay.utils;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import org.mifos.mobilewallet.mifospay.R;
+
+
+public class DialogBox {
+
+    public AlertDialog alertDialog;
+    public DialogInterface.OnClickListener onPositiveListener;
+    public DialogInterface.OnClickListener onNegativeListener;
+
+    public void setOnPositiveListener(final DialogInterface.OnClickListener onPositiveListener) {
+        this.onPositiveListener = onPositiveListener;
+    }
+
+    public void setOnNegativeListener(final DialogInterface.OnClickListener onNegativeListener) {
+        this.onNegativeListener = onNegativeListener;
+    }
+
+    public void show(final Context context, int title, int message, int positive,
+                     int negative) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AppTheme_Dialog);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(
+                positive,
+                onPositiveListener);
+
+        builder.setNegativeButton(
+                negative,
+                onNegativeListener);
+
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+    public void dismiss() {
+        alertDialog.dismiss();
+    }
+}
