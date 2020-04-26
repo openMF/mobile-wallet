db010f611d015e219247e29e2469408e3e5f0b82
Shobhit Goswami <sg.shobhit.1@gmail.com>
2020-04-26 22:15:33 +0530

    fix: show Toast when debit card number is not in range (12-19)(#898)

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/bank/presenter/DebitCardPresenter.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/bank/presenter/DebitCardPresenter.java
@@ -1,9 +1,12 @@
 package org.mifos.mobilewallet.mifospay.bank.presenter;
 
+import org.mifos.mobilewallet.mifospay.R;
 import org.mifos.mobilewallet.mifospay.bank.BankContract;
 import org.mifos.mobilewallet.mifospay.base.BaseView;
 
 import javax.inject.Inject;
+
+import static org.mifos.mobilewallet.mifospay.MifosPayApp.getContext;
 
 /**
  * Created by ankur on 13/July/2018
@@ -27,8 +30,11 @@ public class DebitCardPresenter implements BankContract.DebitCardPresenter {
     @Override
     public void verifyDebitCard(String s, String s1, String s2) {
         String otp = "0000";
-        mDebitCardView.verifyDebitCardSuccess(otp);
-//        String message = "error";
-//        mDebitCardView.verifyDebitCardError(message);
+        if ((s.length() < 12) || (s.length() > 19)) {
+            mDebitCardView.verifyDebitCardError(getContext()
+                    .getString(R.string.debit_card_error_message));
+        } else {
+            mDebitCardView.verifyDebitCardSuccess(otp);
+        }
     }
 }
