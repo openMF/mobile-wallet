6ab9f4b1fc8bcfa1533411ce3f2dd7ee2e288739
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2020-05-19 03:52:43 +0530

    fix: Removed multiple entries of LoginActivity on BackStack

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/auth/ui/LoginActivity.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/auth/ui/LoginActivity.java
@@ -191,7 +191,6 @@ public class LoginActivity extends BaseActivity implements AuthContract.LoginVie
                 DebugUtil.log(Constants.GOOGLE_SIGN_IN_FAILED, e.getMessage());
                 Toaster.showToast(this, Constants.GOOGLE_SIGN_IN_FAILED);
                 hideProgressDialog();
-                startActivity(new Intent(LoginActivity.this, LoginActivity.class));
             }
         }
     }
