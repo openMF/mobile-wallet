8bc8f103b4ce70c7e8b2ee5c39f08b6a8306a0d9
aakashgoel2002 <65633502+aakashgoel2002@users.noreply.github.com>
2020-12-06 23:40:44 +0530

    Update ExampleInstrumentedTest.java

--- core/src/androidTest/java/org/mifos/mobilewallet/core/ExampleInstrumentedTest.java
+++ core/src/androidTest/java/org/mifos/mobilewallet/core/ExampleInstrumentedTest.java
@@ -1,17 +1,10 @@
 package org.mifos.mobilewallet.core;
-
 import static org.junit.Assert.assertEquals;
-
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
+import android.support.test.*;
+import org.junit.*;
 /**
  * Instrumentation test, which will execute on an Android device.
- *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
@@ -20,7 +13,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("org.mifos.mobilewallet.core.test", appContext.getPackageName());
     }
 }
