4ec8a16d6f237fcb04de18ecec56cd4c811c2869
Mrudul Tora <mrudultora@gmail.com>
2020-12-10 18:19:07 +0530

    Added a Toast when no of the sim is selected and confirm is pressed

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/bank/ui/ChooseSimDialog.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/bank/ui/ChooseSimDialog.java
@@ -8,6 +8,7 @@ import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.mifos.mobilewallet.mifospay.R;
 
@@ -75,8 +76,10 @@ public class ChooseSimDialog extends BottomSheetDialogFragment {
     @OnClick(R.id.btn_confirm)
     public void onConfirmClicked() {
         dismiss();
-        if (getActivity() instanceof LinkBankAccountActivity) {
+        if (getActivity() instanceof LinkBankAccountActivity && (selectedSim==1 || selectedSim==2)) {
             ((LinkBankAccountActivity) getActivity()).linkBankAccount(selectedSim);
+        } else {
+            Toast.makeText(getContext().getApplicationContext(),getString(R.string.choose_sim),Toast.LENGTH_SHORT).show();
         }
     }
 }
