79e3631adc9f5f71005bd8da0fe02e6fa97a23b7
Avinash Vijayvargiya <avinash14022002@gmail.com>
2022-04-06 01:41:47 +0530

    Migrated to kotlin domain package mifospay

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/domain/model/Bank.kt
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/domain/model/Bank.kt
@@ -1,32 +1,9 @@
-package org.mifos.mobilewallet.mifospay.domain.model;
+package org.mifos.mobilewallet.mifospay.domain.model
 
 /**
  * Created by naman on 20/6/17.
  */
-
-public class Bank {
-
-    private String name;
-    private int image;
-
-    // 0= popular, 1 = other
-    private int type;
-
-    public Bank(String name, int image, int type) {
-        this.name = name;
-        this.type = type;
-        this.image = image;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getImage() {
-        return image;
-    }
-
-    public int getType() {
-        return type;
-    }
-}
+class Bank(
+    val name: String, val image: Int, // 0= popular, 1 = other
+    val type: Int
+)
