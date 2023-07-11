afa8d5b2699e2e285beeace8458af794a07ad4c8
Rachit Gupta <guptarachit2004@gmail.com>
2023-07-11 19:45:28 +0530

    Kotlin conversion: Request Fragment

---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/payments/presenter/TransferPresenter.kt
@@ -0,0 +1,63 @@
+package org.mifos.mobilewallet.mifospay.payments.presenter
+
+import org.mifos.mobilewallet.core.base.UseCase.UseCaseCallback
+import org.mifos.mobilewallet.core.base.UseCaseHandler
+import org.mifos.mobilewallet.core.domain.usecase.account.FetchAccount
+import org.mifos.mobilewallet.core.domain.usecase.client.FetchClientData
+import org.mifos.mobilewallet.mifospay.base.BaseView
+import org.mifos.mobilewallet.mifospay.data.local.LocalRepository
+import org.mifos.mobilewallet.mifospay.home.BaseHomeContract
+import org.mifos.mobilewallet.mifospay.utils.Constants
+import javax.inject.Inject
+
+/**
+ * Created by naman on 30/8/17.
+ */
+class TransferPresenter @Inject constructor(
+    private val mUsecaseHandler: UseCaseHandler,
+    private val localRepository: LocalRepository
+) :
+    BaseHomeContract.TransferPresenter {
+    @Inject
+    var fetchClientData: FetchClientData? = null
+
+    @Inject
+    var mFetchAccount: FetchAccount? = null
+    private var mTransferView: BaseHomeContract.TransferView? = null
+    override fun attachView(baseView: BaseView<*>?) {
+        mTransferView = baseView as BaseHomeContract.TransferView?
+        mTransferView!!.setPresenter(this)
+    }
+
+    override fun fetchVpa() {
+        mTransferView!!.showVpa(localRepository.clientDetails.externalId)
+    }
+
+    override fun fetchMobile() {
+        mTransferView!!.showMobile(localRepository.preferencesHelper.mobile)
+    }
+
+    override fun checkSelfTransfer(externalId: String?): Boolean {
+        return externalId == localRepository.clientDetails.externalId
+    }
+
+    override fun checkBalanceAvailability(externalId: String?, transferAmount: Double) {
+        mUsecaseHandler.execute(mFetchAccount,
+            FetchAccount.RequestValues(localRepository.clientDetails.clientId),
+            object : UseCaseCallback<FetchAccount.ResponseValue> {
+                override fun onSuccess(response: FetchAccount.ResponseValue) {
+                    mTransferView!!.hideSwipeProgress()
+                    if (transferAmount > response.account.balance) {
+                        mTransferView!!.showSnackbar(Constants.INSUFFICIENT_BALANCE)
+                    } else {
+                        mTransferView!!.showClientDetails(externalId, transferAmount)
+                    }
+                }
+
+                override fun onError(message: String) {
+                    mTransferView!!.hideSwipeProgress()
+                    mTransferView!!.showToast(Constants.ERROR_FETCHING_BALANCE)
+                }
+            })
+    }
+}
---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/payments/ui/SendFragment.kt
@@ -0,0 +1,331 @@
+package org.mifos.mobilewallet.mifospay.payments.ui
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.os.Build
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.support.design.chip.Chip
+import android.support.design.widget.TextInputLayout
+import android.support.transition.TransitionManager
+import android.support.v4.content.ContextCompat
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
+import org.mifos.mobilewallet.mifospay.R
+import org.mifos.mobilewallet.mifospay.base.BaseActivity
+import org.mifos.mobilewallet.mifospay.base.BaseFragment
+import org.mifos.mobilewallet.mifospay.common.ui.MakeTransferFragment
+import org.mifos.mobilewallet.mifospay.home.BaseHomeContract
+import org.mifos.mobilewallet.mifospay.payments.presenter.TransferPresenter
+import org.mifos.mobilewallet.mifospay.qr.ui.ReadQrActivity
+import org.mifos.mobilewallet.mifospay.utils.Constants
+import org.mifos.mobilewallet.mifospay.utils.Toaster
+import org.mifos.mobilewallet.mifospay.utils.Utils.hideSoftKeyboard
+import javax.inject.Inject
+
+/**
+ * Created by naman on 30/8/17.
+ */
+class SendFragment : BaseFragment(), BaseHomeContract.TransferView {
+    @Inject
+    var mPresenter: TransferPresenter? = null
+    var mTransferPresenter: BaseHomeContract.TransferPresenter? = null
+
+    @BindView(R.id.rl_send_container)
+    var sendContainer: ViewGroup? = null
+
+    @BindView(R.id.et_amount)
+    var etAmount: EditText? = null
+
+    @BindView(R.id.et_vpa)
+    var etVpa: EditText? = null
+
+    @BindView(R.id.btn_submit)
+    var btnTransfer: Button? = null
+
+    @BindView(R.id.btn_scan_qr)
+    var btnScanQr: TextView? = null
+
+    @BindView(R.id.btn_vpa)
+    var mBtnVpa: Chip? = null
+
+    @BindView(R.id.btn_mobile)
+    var mBtnMobile: Chip? = null
+
+    @BindView(R.id.et_mobile_number)
+    var mEtMobileNumber: EditText? = null
+
+    @BindView(R.id.btn_search_contact)
+    var mBtnSearchContact: TextView? = null
+
+    @BindView(R.id.rl_mobile)
+    var mRlMobile: RelativeLayout? = null
+
+    @BindView(R.id.til_vpa)
+    var mTilVpa: TextInputLayout? = null
+    private var vpa: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as BaseActivity?)!!.activityComponent.inject(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(
+            R.layout.fragment_send, container,
+            false
+        )
+        ButterKnife.bind(this, rootView)
+        setSwipeEnabled(false)
+        mPresenter!!.attachView(this)
+        mEtMobileNumber!!.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+        mBtnVpa!!.isSelected = true
+        return rootView
+    }
+
+    @OnClick(R.id.btn_vpa)
+    fun onVPASelected() {
+        TransitionManager.beginDelayedTransition(sendContainer!!)
+        mBtnVpa!!.isSelected = true
+        mBtnVpa!!.isFocusable = true
+        mBtnVpa!!.setChipBackgroundColorResource(R.color.clickedblue)
+        mBtnMobile!!.isSelected = false
+        mBtnMobile!!.setChipBackgroundColorResource(R.color.changedBackgroundColour)
+        btnScanQr!!.visibility = View.VISIBLE
+        mRlMobile!!.visibility = View.GONE
+        mTilVpa!!.visibility = View.VISIBLE
+        hideSoftKeyboard(activity!!)
+    }
+
+    @OnClick(R.id.btn_mobile)
+    fun onMobileSelected() {
+        TransitionManager.beginDelayedTransition(sendContainer!!)
+        mBtnMobile!!.isSelected = true
+        mBtnMobile!!.isFocusable = true
+        mBtnMobile!!.setChipBackgroundColorResource(R.color.clickedblue)
+        mBtnVpa!!.isSelected = false
+        mBtnVpa!!.setChipBackgroundColorResource(R.color.changedBackgroundColour)
+        mTilVpa!!.visibility = View.GONE
+        btnScanQr!!.visibility = View.GONE
+        mRlMobile!!.visibility = View.VISIBLE
+        hideSoftKeyboard(activity!!)
+    }
+
+    @OnClick(R.id.btn_submit)
+    fun transferClicked() {
+        val externalId = etVpa!!.text.toString().trim { it <= ' ' }
+        val eamount = etAmount!!.text.toString().trim { it <= ' ' }
+        val mobileNumber = mEtMobileNumber!!.text
+            .toString().trim { it <= ' ' }.replace("\\s+".toRegex(), "")
+        if ((eamount.isEmpty() || mBtnVpa!!.isSelected && externalId.isEmpty()) || mBtnMobile!!.isSelected && mobileNumber == "") {
+            Toast.makeText(
+                activity,
+                Constants.PLEASE_ENTER_ALL_THE_FIELDS, Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            val amount = eamount.toDouble()
+            if (amount <= 0) {
+                showSnackbar(Constants.PLEASE_ENTER_VALID_AMOUNT)
+                return
+            }
+            if (externalId != Constants.VPA_VALIDATION_REGEX) {
+                showSnackbar(getString(R.string.please_enter_valid_vpa))
+                return
+            }
+            if (!mTransferPresenter!!.checkSelfTransfer(externalId)) {
+                mTransferPresenter!!.checkBalanceAvailability(externalId, amount)
+            } else {
+                showSnackbar(Constants.SELF_ACCOUNT_ERROR)
+            }
+        }
+    }
+
+    @OnClick(R.id.btn_scan_qr)
+    fun scanQrClicked() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(
+                activity!!,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // Permission is not granted
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA)
+        } else {
+
+            // Permission has already been granted
+            val i = Intent(activity, ReadQrActivity::class.java)
+            startActivityForResult(i, SCAN_QR_REQUEST_CODE)
+        }
+    }
+
+    override fun showVpa(vpa: String?) {
+        this.vpa = vpa
+    }
+
+    override fun setPresenter(presenter: BaseHomeContract.TransferPresenter?) {
+        mTransferPresenter = presenter
+    }
+
+    @OnClick(R.id.btn_search_contact)
+    fun searchContactClicked() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(
+                activity!!,
+                Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // Permission is not granted
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                REQUEST_READ_CONTACTS
+            )
+        } else {
+
+            // Permission has already been granted
+            val intent = Intent(
+                Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            )
+            startActivityForResult(intent, PICK_CONTACT)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == SCAN_QR_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val qrData = data.getStringExtra(Constants.QR_DATA)
+            val qrDataArray = qrData!!.split(", ".toRegex()).dropLastWhile { it.isEmpty() }
+                .toTypedArray()
+            if (qrDataArray.size == 1) {
+                etVpa!!.setText(qrDataArray[0])
+            } else {
+                etVpa!!.setText(qrDataArray[0])
+                etAmount!!.setText(qrDataArray[1])
+            }
+            val externalId = etVpa!!.text.toString().trim { it <= ' ' }
+            if (etAmount!!.text.toString().isEmpty()) {
+                showSnackbar(Constants.PLEASE_ENTER_AMOUNT)
+                return
+            }
+            val amount = etAmount!!.text.toString().toDouble()
+            if (!mTransferPresenter!!.checkSelfTransfer(externalId)) {
+                mTransferPresenter!!.checkBalanceAvailability(externalId, amount)
+            } else {
+                showSnackbar(Constants.SELF_ACCOUNT_ERROR)
+            }
+        } else if (requestCode == PICK_CONTACT && resultCode == Activity.RESULT_OK) {
+            var cursor: Cursor? = null
+            try {
+                var phoneNo: String? = null
+                var name: String? = null
+                // getData() method will have the Content Uri of the selected contact
+                val uri = data.data
+                //Query the content uri
+                cursor = context!!.contentResolver.query(uri!!, null, null, null, null)
+                cursor!!.moveToFirst()
+                // column index of the phone number
+                val phoneIndex = cursor.getColumnIndex(
+                    ContactsContract.CommonDataKinds.Phone.NUMBER
+                )
+                // column index of the contact name
+                val nameIndex = cursor.getColumnIndex(
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                )
+                phoneNo = cursor.getString(phoneIndex)
+                name = cursor.getString(nameIndex)
+                mEtMobileNumber!!.setText(phoneNo)
+            } catch (e: Exception) {
+                showToast(Constants.ERROR_CHOOSING_CONTACT)
+            }
+        } else if (requestCode == REQUEST_SHOW_DETAILS && resultCode == Activity.RESULT_CANCELED) {
+            if (mBtnMobile!!.isSelected) {
+                showSnackbar(Constants.ERROR_FINDING_MOBILE_NUMBER)
+            } else {
+                showSnackbar(Constants.ERROR_FINDING_VPA)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CAMERA -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // permission was granted, yay! Do the
+                    // camera-related task you need to do.
+                    val i = Intent(activity, ReadQrActivity::class.java)
+                    startActivityForResult(i, SCAN_QR_REQUEST_CODE)
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toaster.show(view, Constants.NEED_CAMERA_PERMISSION_TO_SCAN_QR_CODE)
+                }
+                return
+            }
+            REQUEST_READ_CONTACTS -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    val intent = Intent(
+                        Intent.ACTION_PICK,
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                    )
+                    startActivityForResult(intent, PICK_CONTACT)
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toaster.show(view, Constants.NEED_READ_CONTACTS_PERMISSION)
+                }
+                return
+            }
+        }
+    }
+
+    override fun showToast(message: String?) {
+        Toaster.showToast(context, message)
+    }
+
+    override fun showSnackbar(message: String?) {
+        Toaster.show(view, message)
+    }
+
+    override fun showMobile(mobileNo: String?) {}
+    override fun showClientDetails(externalId: String?, amount: Double) {
+        val fragment = MakeTransferFragment.newInstance(externalId, amount)
+        fragment.setTargetFragment(this, REQUEST_SHOW_DETAILS)
+        if (parentFragment != null) {
+            fragment.show(
+                parentFragment!!.childFragmentManager,
+                Constants.MAKE_TRANSFER_FRAGMENT
+            )
+        }
+    }
+
+    companion object {
+        const val REQUEST_SHOW_DETAILS = 3
+        private const val REQUEST_CAMERA = 0
+        private const val SCAN_QR_REQUEST_CODE = 666
+        private const val PICK_CONTACT = 1
+        private const val REQUEST_READ_CONTACTS = 2
+    }
+}
