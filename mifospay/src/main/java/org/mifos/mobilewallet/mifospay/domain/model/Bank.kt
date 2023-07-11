4075199fcd46c57c912be7a27ed2e58031710198
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2023-07-11 19:52:26 +0530

    Kotlin conversion: mifospay domain (#1300)
    * Rename .java to .kt
    
    * Migrated to kotlin domain package mifospay

---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/domain/model/Bank.kt
@@ -0,0 +1,9 @@
+package org.mifos.mobilewallet.mifospay.domain.model
+
+/**
+ * Created by naman on 20/6/17.
+ */
+class Bank(
+    val name: String, val image: Int, // 0= popular, 1 = other
+    val type: Int
+)
