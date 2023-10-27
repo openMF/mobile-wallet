62c4707504ad16db6a7cdddabb4733a9f251dc06
Rajan Maurya <rajanmaurya154@gmail.com>
2023-10-27 15:41:34 +0530

    fix: Splash screen using SplashAPI

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/SplashScreenActivity.kt
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/SplashScreenActivity.kt
@@ -1,25 +1,31 @@
-package org.mifos.mobilewallet.mifospay;
+package org.mifos.mobilewallet.mifospay
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.os.Handler;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import org.mifos.mobilewallet.mifospay.auth.ui.LoginActivity;
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.mifos.mobilewallet.mifospay.auth.ui.LoginActivity
 
-public class SplashScreenActivity extends AppCompatActivity {
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : ComponentActivity() {
 
-    private final int splashScreenDuration = 2000;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
-                finish();
-            }
-        }, splashScreenDuration);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
+        super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        lifecycleScope.launch {
+            delay(2000)
+            startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
+            finish()
+        }
     }
 }
