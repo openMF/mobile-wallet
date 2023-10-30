6836f103eb3301714d0da444fd9530945a3d0192
Rajan Maurya <rajanmaurya154@gmail.com>
2023-10-30 11:46:55 +0530

    feat: convert login into jetpack compose

---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/theme/Color.kt
@@ -0,0 +1,15 @@
+package org.mifos.mobilewallet.mifospay.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// colors
+val black = Color(0xFF000000)
+val grey = Color(0xFF757074)
