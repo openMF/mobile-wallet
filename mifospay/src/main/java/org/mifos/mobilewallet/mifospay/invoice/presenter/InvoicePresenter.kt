ef1abdbca412035e69dc654adf15d6911a940146
Pratyush Singh <aries.pratyush@gmail.com>
2024-01-08 12:39:04 +0530

    refactor #1458: migrated invoice from java to kotlin

---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/invoice/presenter/InvoicePresenter.kt
@@ -0,0 +1,46 @@
+package org.mifos.mobilewallet.mifospay.invoice.presenter
+
+import android.net.Uri
+import org.mifos.mobilewallet.core.base.UseCase.UseCaseCallback
+import org.mifos.mobilewallet.core.base.UseCaseHandler
+import org.mifos.mobilewallet.core.domain.usecase.invoice.FetchInvoice
+import org.mifos.mobilewallet.mifospay.base.BaseView
+import org.mifos.mobilewallet.mifospay.data.local.PreferencesHelper
+import org.mifos.mobilewallet.mifospay.invoice.InvoiceContract
+import org.mifos.mobilewallet.mifospay.invoice.InvoiceContract.InvoiceView
+import javax.inject.Inject
+
+/**
+ * Created by ankur on 07/June/2018
+ */
+class InvoicePresenter @Inject constructor(
+    private val mUseCaseHandler: UseCaseHandler,
+    private val mPreferencesHelper: PreferencesHelper
+) : InvoiceContract.InvoicePresenter {
+    var mInvoiceView: InvoiceView? = null
+
+    @JvmField
+    @Inject
+    var fetchInvoiceUseCase: FetchInvoice? = null
+    override fun attachView(baseView: BaseView<*>?) {
+        mInvoiceView = baseView as InvoiceView?
+        mInvoiceView!!.setPresenter(this)
+    }
+
+    override fun getInvoiceDetails(data: Uri?) {
+        mUseCaseHandler.execute(fetchInvoiceUseCase, FetchInvoice.RequestValues(data),
+            object : UseCaseCallback<FetchInvoice.ResponseValue?> {
+                override fun onSuccess(response: FetchInvoice.ResponseValue?) {
+                    mInvoiceView!!.showInvoiceDetails(
+                        response?.invoices?.get(0),
+                        mPreferencesHelper.fullName + " "
+                                + mPreferencesHelper.clientId, data.toString()
+                    )
+                }
+
+                override fun onError(message: String) {
+                    mInvoiceView!!.showToast(message)
+                }
+            })
+    }
+}
