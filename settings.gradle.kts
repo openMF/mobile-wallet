515705e421d342a06a9a7f9268a01969a575f979
Rajan Maurya <rajanmaurya154@gmail.com>
2024-01-13 18:24:35 -0500

    feat: kotlin to dsl

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://www.jitpack.io")
+        maven("https://plugins.gradle.org/m2/")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://www.jitpack.io")
+        maven("https://plugins.gradle.org/m2/")
+    }
+}
+rootProject.name = "Mobile-Wallet"
+
+include(":mifospay")
+include(":core")
