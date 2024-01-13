1c3379f1aaeb31422d5fd7a959a3cb988fec4a69
Pratyush Singh <aries.pratyush@gmail.com>
2024-01-13 18:08:54 +0530

    refactor #1470: migrated bank package from java to kotlin

---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/bank/fragment/DebitCardFragment.kt
@@ -0,0 +1,123 @@
+package org.mifos.mobilewallet.mifospay.bank.fragment
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView.OnEditorActionListener
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.alimuzaffar.lib.pin.PinEntryEditText
+import dagger.hilt.android.AndroidEntryPoint
+import org.mifos.mobilewallet.mifospay.R
+import org.mifos.mobilewallet.mifospay.bank.BankContract
+import org.mifos.mobilewallet.mifospay.bank.BankContract.DebitCardView
+import org.mifos.mobilewallet.mifospay.bank.presenter.DebitCardPresenter
+import org.mifos.mobilewallet.mifospay.bank.ui.SetupUpiPinActivity
+import org.mifos.mobilewallet.mifospay.base.BaseFragment
+import org.mifos.mobilewallet.mifospay.utils.Constants
+import org.mifos.mobilewallet.mifospay.utils.Toaster
+import javax.inject.Inject
+
+/**
+ * Created by ankur on 13/July/2018
+ */
+@AndroidEntryPoint
+class DebitCardFragment : BaseFragment(), DebitCardView {
+    @JvmField
+    @Inject
+    var mPresenter: DebitCardPresenter? = null
+    var mDebitCardPresenter: BankContract.DebitCardPresenter? = null
+
+    @JvmField
+    @BindView(R.id.et_debit_card_number)
+    var mEtDebitCardNumber: EditText? = null
+
+    @JvmField
+    @BindView(R.id.pe_month)
+    var mPeMonth: PinEntryEditText? = null
+
+    @JvmField
+    @BindView(R.id.pe_year)
+    var mPeYear: PinEntryEditText? = null
+    var key = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val rootView = inflater.inflate(
+            R.layout.fragment_debit_card,
+            container, false
+        ) as ViewGroup
+        ButterKnife.bind(this, rootView)
+        mPresenter!!.attachView(this)
+        mPeYear!!.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                okayClicked()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+        mPeMonth!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.length == 2) {
+                    mPeYear!!.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+        mPeYear!!.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_DEL && mPeYear!!.length() == 0) {
+                if (key) {
+                    mPeMonth!!.requestFocus()
+                    mPeMonth!!.dispatchKeyEvent(
+                        KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL)
+                    )
+                    key = false
+                    return@OnKeyListener false
+                }
+                key = true
+            }
+            false
+        })
+        return rootView
+    }
+
+    private fun okayClicked() {
+        showProgressDialog(Constants.PLEASE_WAIT)
+        mDebitCardPresenter!!.verifyDebitCard(mEtDebitCardNumber!!.text
+            .toString().trim { it <= ' ' },
+            mPeMonth!!.text.toString(),
+            mPeYear!!.text.toString()
+        )
+    }
+
+    override fun verifyDebitCardSuccess(otp: String?) {
+        hideProgressDialog()
+        if (activity is SetupUpiPinActivity) {
+            (activity as SetupUpiPinActivity?)!!.debitCardVerified(otp)
+        }
+    }
+
+    override fun verifyDebitCardError(message: String?) {
+        hideProgressDialog()
+        mEtDebitCardNumber!!.requestFocusFromTouch()
+        showToast(message)
+    }
+
+    override fun setPresenter(presenter: BankContract.DebitCardPresenter?) {
+        mDebitCardPresenter = presenter
+    }
+
+    fun showToast(message: String?) {
+        Toaster.showToast(activity, message)
+    }
+}
---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/bank/fragment/OtpFragment.kt
@@ -0,0 +1,81 @@
+package org.mifos.mobilewallet.mifospay.bank.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import android.widget.TextView.OnEditorActionListener
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.alimuzaffar.lib.pin.PinEntryEditText
+import dagger.hilt.android.AndroidEntryPoint
+import org.mifos.mobilewallet.mifospay.R
+import org.mifos.mobilewallet.mifospay.bank.ui.SetupUpiPinActivity
+import org.mifos.mobilewallet.mifospay.base.BaseFragment
+import org.mifos.mobilewallet.mifospay.utils.Constants
+import org.mifos.mobilewallet.mifospay.utils.Toaster
+
+/**
+ * Created by ankur on 13/July/2018
+ */
+@AndroidEntryPoint
+class OtpFragment : BaseFragment() {
+    @JvmField
+    @BindView(R.id.tv_title)
+    var mTvTitle: TextView? = null
+
+    @JvmField
+    @BindView(R.id.pe_otp)
+    var mPeOtp: PinEntryEditText? = null
+    private var otp: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            otp = requireArguments().getString(Constants.OTP)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val rootView = inflater.inflate(R.layout.fragment_otp, container, false) as ViewGroup
+        ButterKnife.bind(this, rootView)
+        mPeOtp!!.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                okayClicked()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+        mPeOtp!!.requestFocus()
+        return rootView
+    }
+
+    fun okayClicked() {
+        if (activity is SetupUpiPinActivity) {
+            if (mPeOtp!!.text.toString() == otp) {
+                (activity as SetupUpiPinActivity?)!!.otpVerified()
+            } else {
+                showToast(getString(R.string.wrong_otp))
+            }
+        }
+    }
+
+    fun showToast(message: String?) {
+        Toaster.showToast(activity, message)
+    }
+
+    companion object {
+        fun newInstance(otp: String?): OtpFragment {
+            val args = Bundle()
+            args.putString(Constants.OTP, otp)
+            val fragment = OtpFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
