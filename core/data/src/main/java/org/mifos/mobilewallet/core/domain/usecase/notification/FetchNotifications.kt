714aaaa987669d3ed70dc90cc8e83a3ed1eac1c6
Pratyush Singh <aries.pratyush@gmail.com>
2024-02-03 20:09:26 +0530

    refactor #1499: migrated notification usecase to kotlin

---
+++ core/data/src/main/java/org/mifos/mobilewallet/core/domain/usecase/notification/FetchNotifications.kt
@@ -0,0 +1,40 @@
+package org.mifos.mobilewallet.core.domain.usecase.notification
+
+import org.mifos.mobilewallet.core.base.UseCase
+import org.mifos.mobilewallet.core.data.fineract.repository.FineractRepository
+import org.mifos.mobilewallet.core.domain.model.NotificationPayload
+import org.mifos.mobilewallet.core.utils.Constants
+import rx.Subscriber
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import javax.inject.Inject
+
+/**
+ * Created by ankur on 24/July/2018
+ */
+class FetchNotifications @Inject constructor(private val mFineractRepository: FineractRepository) :
+    UseCase<FetchNotifications.RequestValues?, FetchNotifications.ResponseValue?>() {
+
+    class RequestValues(val clientId: Long) : UseCase.RequestValues
+    class ResponseValue(
+        val notificationPayloadList: List<NotificationPayload?>
+    ) : UseCase.ResponseValue
+
+    override fun executeUseCase(requestValues: RequestValues?) {
+        if (requestValues != null) {
+            mFineractRepository.fetchNotifications(requestValues.clientId)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(object : Subscriber<List<NotificationPayload?>?>() {
+                    override fun onCompleted() {}
+                    override fun onError(e: Throwable) {
+                        useCaseCallback.onError(Constants.ERROR_FETCHING_NOTIFICATIONS)
+                    }
+
+                    override fun onNext(notificationPayloads: List<NotificationPayload?>?) {
+                        useCaseCallback.onSuccess(notificationPayloads?.let { ResponseValue(it) })
+                    }
+                })
+        }
+    }
+}
