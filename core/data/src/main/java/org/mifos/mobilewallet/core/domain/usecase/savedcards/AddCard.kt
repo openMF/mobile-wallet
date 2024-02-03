640b2e884fb382896535f11adb99ea602af86344
Pratyush Singh <aries.pratyush@gmail.com>
2024-02-03 20:03:54 +0530

    refactor #1497: migrated twofactor package to kotlin

---
+++ core/data/src/main/java/org/mifos/mobilewallet/core/domain/usecase/savedcards/AddCard.kt
@@ -0,0 +1,37 @@
+package org.mifos.mobilewallet.core.domain.usecase.savedcards
+
+import org.mifos.mobilewallet.core.base.UseCase
+import org.mifos.mobilewallet.core.data.fineract.api.GenericResponse
+import org.mifos.mobilewallet.core.data.fineract.entity.savedcards.Card
+import org.mifos.mobilewallet.core.data.fineract.repository.FineractRepository
+import rx.Subscriber
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import javax.inject.Inject
+
+/**
+ * Created by ankur on 21/May/2018
+ */
+class AddCard @Inject constructor(private val mFineractRepository: FineractRepository) :
+    UseCase<AddCard.RequestValues?, AddCard.ResponseValue?>() {
+    class RequestValues(val clientId: Long, val card: Card) : UseCase.RequestValues
+    class ResponseValue : UseCase.ResponseValue
+
+    override fun executeUseCase(requestValues: RequestValues?) {
+        if (requestValues != null) {
+            mFineractRepository.addSavedCards(requestValues.clientId, requestValues.card)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(object : Subscriber<GenericResponse?>() {
+                    override fun onCompleted() {}
+                    override fun onError(e: Throwable) {
+                        useCaseCallback.onError(e.toString())
+                    }
+
+                    override fun onNext(t: GenericResponse?) {
+                        useCaseCallback.onSuccess(ResponseValue())
+                    }
+                })
+        }
+    }
+}
