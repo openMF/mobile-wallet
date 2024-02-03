3775e1f76d9f094c6c1ff02dbd07a76cd8be478a
Pratyush Singh <aries.pratyush@gmail.com>
2024-02-03 20:55:44 +0530

    refactor #1497: migrated savedcards usecase to kotlin

--- core/data/src/main/java/org/mifos/mobilewallet/core/domain/usecase/savedcards/EditCard.kt
+++ core/data/src/main/java/org/mifos/mobilewallet/core/domain/usecase/savedcards/EditCard.kt
@@ -13,26 +13,25 @@ import javax.inject.Inject
  * Created by ankur on 22/May/2018
  */
 class EditCard @Inject constructor(private val mFineractRepository: FineractRepository) :
-    UseCase<EditCard.RequestValues?, EditCard.ResponseValue?>() {
+    UseCase<EditCard.RequestValues, EditCard.ResponseValue>() {
 
     class RequestValues(val clientId: Int, val card: Card) : UseCase.RequestValues
     class ResponseValue : UseCase.ResponseValue
 
-    override fun executeUseCase(requestValues: RequestValues?) {
-        if (requestValues != null) {
-            mFineractRepository.editSavedCard(requestValues.clientId, requestValues.card)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(object : Subscriber<GenericResponse?>() {
-                    override fun onCompleted() {}
-                    override fun onError(e: Throwable) {
-                        useCaseCallback.onError(e.toString())
-                    }
+    override fun executeUseCase(requestValues: RequestValues) {
+        mFineractRepository.editSavedCard(requestValues.clientId, requestValues.card)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(object : Subscriber<GenericResponse?>() {
+                override fun onCompleted() {}
+                override fun onError(e: Throwable) {
+                    useCaseCallback.onError(e.toString())
+                }
 
-                    override fun onNext(t: GenericResponse?) {
-                        useCaseCallback.onSuccess(ResponseValue())
-                    }
-                })
-        }
+                override fun onNext(t: GenericResponse?) {
+                    useCaseCallback.onSuccess(ResponseValue())
+                }
+            })
+
     }
 }
--- core/data/src/main/java/org/mifos/mobilewallet/core/domain/usecase/twofactor/ValidateOTP.kt
+++ core/data/src/main/java/org/mifos/mobilewallet/core/domain/usecase/twofactor/ValidateOTP.kt
@@ -12,26 +12,25 @@ import javax.inject.Inject
  * Created by ankur on 01/June/2018
  */
 class ValidateOTP @Inject constructor(private val mFineractRepository: FineractRepository) :
-    UseCase<ValidateOTP.RequestValues?, ValidateOTP.ResponseValue?>() {
+    UseCase<ValidateOTP.RequestValues, ValidateOTP.ResponseValue>() {
 
     class RequestValues(val token: String) : UseCase.RequestValues
     class ResponseValue(val accessToken: AccessToken) : UseCase.ResponseValue
 
-    override fun executeUseCase(requestValues: RequestValues?) {
-        if (requestValues != null) {
-            mFineractRepository.validateToken(requestValues.token)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(object : Subscriber<AccessToken?>() {
-                    override fun onCompleted() {}
-                    override fun onError(e: Throwable) {
-                        useCaseCallback.onError(e.toString())
-                    }
+    override fun executeUseCase(requestValues: RequestValues) {
+        mFineractRepository.validateToken(requestValues.token)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(object : Subscriber<AccessToken>() {
+                override fun onCompleted() {}
+                override fun onError(e: Throwable) {
+                    useCaseCallback.onError(e.toString())
+                }
 
-                    override fun onNext(accessToken: AccessToken?) {
-                        useCaseCallback.onSuccess(accessToken?.let { ResponseValue(it) })
-                    }
-                })
-        }
+                override fun onNext(accessToken: AccessToken?) {
+                    useCaseCallback.onSuccess(accessToken?.let { ResponseValue(it) })
+                }
+            })
+
     }
 }
