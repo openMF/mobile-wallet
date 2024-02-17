94d9a14e7e51b732c452d9b14c632024499c003a
Rajan Maurya <rajanmaurya154@gmail.com>
2024-02-16 22:16:15 -0500

    fix: Add login viewmodel

---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/auth/LoginViewModel.kt
@@ -0,0 +1,13 @@
+package org.mifos.mobilewallet.mifospay.auth
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+
+) : ViewModel() {
+
+
+}
