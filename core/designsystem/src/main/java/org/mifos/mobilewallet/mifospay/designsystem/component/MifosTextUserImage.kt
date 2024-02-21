ae1389ced606f9f782524901e5262603a04af7b2
Pratyush Singh <aries.pratyush@gmail.com>
2024-02-22 00:58:17 +0530

    refactor #1532: migrated profile screen to compose

---
+++ core/designsystem/src/main/java/org/mifos/mobilewallet/mifospay/designsystem/component/MifosTextUserImage.kt
@@ -0,0 +1,32 @@
+package org.mifos.mobilewallet.mifospay.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MifosTextUserImage(modifier: Modifier = Modifier, text: String, size: Dp = 100.dp) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+            .background(color = MaterialTheme.colorScheme.primary),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onPrimary,
+            fontSize = with(LocalDensity.current) { (size / 2).toSp() }
+        )
+    }
+}
---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/home/ui/ProfileScreen.kt
@@ -0,0 +1,143 @@
+package org.mifos.mobilewallet.mifospay.home.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import org.mifos.mobilewallet.mifospay.R
+import org.mifos.mobilewallet.mifospay.home.presenter.ProfileUiState
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun ProfileScreen(
+    profileState: ProfileUiState,
+    onEditProfile: () -> Unit,
+    onSettings: () -> Unit,
+) {
+    var showDetails by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        when (profileState) {
+            ProfileUiState.Loading -> {}
+            is ProfileUiState.Success -> {
+                ProfileImage(bitmap = profileState.bitmapImage)
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp, bottom = 8.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = profileState.name.toString(),
+                        style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Medium),
+                    )
+                    IconButton(onClick = { showDetails = !showDetails }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowDropDown,
+                            tint = Color.Black,
+                            contentDescription = null
+                        )
+                    }
+                }
+
+                if (showDetails) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        DetailItem(
+                            label = stringResource(id = R.string.email),
+                            value = profileState.email.toString()
+                        )
+                        DetailItem(
+                            label = stringResource(id = R.string.vpa),
+                            value = profileState.vpa.toString()
+                        )
+                        DetailItem(
+                            label = stringResource(id = R.string.mobile),
+                            value = profileState.mobile.toString()
+                        )
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 24.dp, end = 24.dp)
+                ) {
+                    FlowRow(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        maxItemsInEachRow = 2
+                    ) {
+                        ProfileItemCard(
+                            modifier = Modifier
+                                .padding(end = 8.dp, bottom = 8.dp)
+                                .weight(1f),
+                            icon = R.drawable.qrcode_black,
+                            text = R.string.personal_qr_code,
+                            onClick = {}
+                        )
+
+                        ProfileItemCard(
+                            modifier = Modifier
+                                .padding(start = 8.dp, bottom = 8.dp)
+                                .weight(1f),
+                            icon = R.drawable.ic_bank,
+                            text = R.string.link_bank_account,
+                            onClick = {}
+                        )
+
+                        ProfileItemCard(
+                            modifier = Modifier
+                                .padding(top = 8.dp, bottom = 8.dp),
+                            icon = R.drawable.ic_contact,
+                            text = R.string.edit_profile,
+                            onClick = { onEditProfile.invoke() }
+                        )
+
+                        ProfileItemCard(
+                            modifier = Modifier
+                                .padding(top = 8.dp),
+                            icon = R.drawable.ic_setting,
+                            text = R.string.settings,
+                            onClick = { onSettings.invoke() }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen(hiltViewModel(), {}, {})
+}
