e9263aeffad2bbacabc4e460b7fe7a0fbf288996
niranjannlc <77341018@nou.edu.np>
2024-02-26 14:03:04 +0545

    added parcelize anotation in remaining class

--- core/model/src/main/java/com/mifos/mobilewallet/model/domain/Transaction.kt
+++ core/model/src/main/java/com/mifos/mobilewallet/model/domain/Transaction.kt
@@ -5,7 +5,9 @@ import android.os.Parcelable
 import com.mifos.mobilewallet.model.domain.Currency
 import com.mifos.mobilewallet.model.domain.TransactionType
 import com.mifos.mobilewallet.model.entity.accounts.savings.TransferDetail
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Transaction(
     var transactionId: String? = null,
     var clientId: Long = 0,
@@ -17,53 +19,5 @@ data class Transaction(
     var transferId: Long = 0,
     var transferDetail: TransferDetail = TransferDetail(),
     var receiptId: String? = null
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readLong(),
-        parcel.readLong(),
-        parcel.readDouble(),
-        parcel.readString(),
-        parcel.readParcelable(Currency::class.java.classLoader)!!,
-        TransactionType.values()[parcel.readInt()],
-        parcel.readLong(),
-        parcel.readParcelable(TransferDetail::class.java.classLoader)!!,
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(transactionId)
-        parcel.writeLong(clientId)
-        parcel.writeLong(accountId)
-        parcel.writeDouble(amount)
-        parcel.writeString(date)
-        parcel.writeParcelable(currency, flags)
-        parcel.writeInt(transactionType.ordinal)
-        parcel.writeLong(transferId)
-        parcel.writeParcelable(transferDetail, flags)
-        parcel.writeString(receiptId)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun toString(): String {
-        return "Transaction{" +
-                "transactionId='" + transactionId + '\'' +
-                ", transferId=" + transferId +
-                ", transferDetail=" + transferDetail +
-                ", receiptId=" + receiptId +
-                '}'
-    }
-
-    companion object CREATOR : Parcelable.Creator<Transaction> {
-        override fun createFromParcel(parcel: Parcel): Transaction {
-            return Transaction(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Transaction?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable  {
+    constructor() : this("", 0, 0, 0.0, "", Currency(), TransactionType.OTHER, 0, TransferDetail(), "")}
--- core/model/src/main/java/com/mifos/mobilewallet/model/domain/user/User.kt
+++ core/model/src/main/java/com/mifos/mobilewallet/model/domain/user/User.kt
@@ -2,35 +2,13 @@ package com.mifos.mobilewallet.model.domain.user
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class User(
     var userId: Long=0L,
     var userName: String?=null,
     var authenticationKey: String?=null
 ) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readLong(),
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeLong(userId)
-        parcel.writeString(userName)
-        parcel.writeString(authenticationKey)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<User> {
-        override fun createFromParcel(parcel: Parcel): User {
-            return User(parcel)
-        }
-
-        override fun newArray(size: Int): Array<User?> {
-            return arrayOfNulls(size)
-        }
-    }
+    constructor() : this(0L, "", "")
 }
