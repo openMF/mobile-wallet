39faeb9df07e4172b82a140763925611d1a21cbf
Aditya <adityakumdale@gmail.com>
2024-04-09 19:44:25 +0530

    Migrated KYCLevel3Fragment to compose

---
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/kyc/presenter/KYCLevel3ViewModel.kt
@@ -0,0 +1,28 @@
+package org.mifos.mobilewallet.mifospay.kyc.presenter
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import org.mifos.mobilewallet.core.base.UseCaseHandler
+import org.mifos.mobilewallet.mifospay.data.local.LocalRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class KYCLevel3ViewModel @Inject constructor(
+    private val mUseCaseHandler: UseCaseHandler,
+    private val mLocalRepository: LocalRepository
+) : ViewModel() {
+    private val _kyc3uiState =
+        MutableStateFlow<KYCLevel3UiState>(KYCLevel3UiState.Loading)
+    val kyc3uiState: StateFlow<KYCLevel3UiState> = _kyc3uiState
+
+    //Todo: Implement KYCLevel3ViewModel flow
+
+}
+
+sealed interface KYCLevel3UiState {
+    data object Loading : KYCLevel3UiState
+    data object Success : KYCLevel3UiState
+    data object Error : KYCLevel3UiState
+}
