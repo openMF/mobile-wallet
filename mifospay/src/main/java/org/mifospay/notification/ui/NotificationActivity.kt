b287f697657634e66bbc788cbc1608827c79d10d
Pratyush Singh <aries.pratyush@gmail.com>
2024-05-07 00:57:55 +0530

    refactor #1620: notification screen to compose

--- mifospay/src/main/java/org/mifospay/notification/ui/NotificationActivity.kt
+++ mifospay/src/main/java/org/mifospay/notification/ui/NotificationActivity.kt
@@ -1,26 +1,10 @@
 package org.mifospay.notification.ui
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.activity.compose.setContent
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import butterknife.BindView
-import butterknife.ButterKnife
 import dagger.hilt.android.AndroidEntryPoint
-import com.mifospay.core.model.domain.NotificationPayload
-import org.mifospay.R
 import org.mifospay.base.BaseActivity
-import org.mifospay.notification.NotificationContract
-import org.mifospay.notification.NotificationContract.NotificationView
-import org.mifospay.notification.presenter.NotificationPresenter
-import org.mifospay.common.Constants
 import org.mifospay.theme.MifosTheme
-import org.mifospay.utils.DebugUtil
-import org.mifospay.utils.Toaster
-import javax.inject.Inject
 
 /**
  * This activity is to view notifications record.
@@ -37,12 +21,5 @@ class NotificationActivity : BaseActivity() {
                 NotificationScreen()
             }
         }
-//        mNotificationPresenter?.fetchNotifications()
     }
-
-//    private fun setupSwipeRefreshLayout() {
-//        setSwipeRefreshEnabled(true)
-//        swipeRefreshLayout?.setOnRefreshListener { mNotificationPresenter?.fetchNotifications() }
-//    }
-
 }
