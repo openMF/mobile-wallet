06fb581988e622e22cb2b83a41f8f5f1ad5f8293
Aditya <adityakumdale@gmail.com>
2024-06-16 20:38:50 +0530

    using BaseActivity

--- feature/receipt/src/main/kotlin/org/mifospay/feature/receipt/ReceiptActivity.kt
+++ feature/receipt/src/main/kotlin/org/mifospay/feature/receipt/ReceiptActivity.kt
@@ -4,15 +4,16 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import com.mifos.mobile.passcode.utils.PassCodeConstants
 import dagger.hilt.android.AndroidEntryPoint
+import org.mifospay.base.BaseActivity
 import org.mifospay.core.designsystem.theme.MifosTheme
 import org.mifospay.feature.passcode.PassCodeActivity
+import org.mifospay.receipt.presenter.ReceiptViewModel
 
 @AndroidEntryPoint
-class ReceiptActivity : AppCompatActivity() {
+class ReceiptActivity : BaseActivity() {
 
     private val receiptViewModel: ReceiptViewModel by viewModels()
     private var deepLinkURI: Uri? = null
@@ -43,5 +44,15 @@ class ReceiptActivity : AppCompatActivity() {
             ),
         )
         finish()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        dismissProgressDialog()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dismissProgressDialog()
     }
 }
