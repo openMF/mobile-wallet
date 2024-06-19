f1e9e74464f5b75f0e996e460006206c909459f6
Aditya Kumdale <95978829+AdityaKumdale@users.noreply.github.com>
2024-06-19 21:41:13 +0530

    Moved history to feature module (#1662)
    * moved history to feature module
    
    * used previews parameters
    
    ---------
    
    Co-authored-by: Rajan Maurya <therajanmaurya@users.noreply.github.com>

---
+++ feature/history/src/main/kotlin/org/mifospay/feature/specific/transactions/SpecificTransactionsActivity.kt
@@ -0,0 +1,43 @@
+package org.mifospay.feature.specific.transactions
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import org.mifospay.common.Constants
+import org.mifospay.core.designsystem.theme.MifosTheme
+import org.mifospay.feature.receipt.ReceiptActivity
+
+@AndroidEntryPoint
+class SpecificTransactionsActivity : AppCompatActivity() {
+
+    private val viewModel: SpecificTransactionsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.setArguments(
+            transactions = intent.getParcelableArrayListExtra(Constants.TRANSACTIONS)
+                ?: arrayListOf(),
+            accountNumber = intent.getStringExtra(Constants.ACCOUNT_NUMBER) ?: ""
+        )
+        setContent {
+            MifosTheme {
+                SpecificTransactionsScreen(
+                    backPress = { onBackPressed() },
+                    transactionItemClicked = { transactionId ->
+                        navigateToReceiptActivity(transactionId)
+                    }
+                )
+            }
+        }
+    }
+
+    private fun navigateToReceiptActivity(transactionId: String) {
+        val intent = Intent(this@SpecificTransactionsActivity, ReceiptActivity::class.java)
+        intent.data = Uri.parse(Constants.RECEIPT_DOMAIN + transactionId)
+        startActivity(intent)
+    }
+}
---
+++ feature/history/src/main/kotlin/org/mifospay/feature/transaction/detail/TransactionDetailViewModel.kt
@@ -0,0 +1,43 @@
+package org.mifospay.feature.transaction.detail
+
+import androidx.lifecycle.ViewModel
+import com.mifospay.core.model.entity.accounts.savings.TransferDetail
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.mifospay.core.data.base.UseCase
+import org.mifospay.core.data.base.UseCaseHandler
+import org.mifospay.core.data.domain.usecase.account.FetchAccountTransfer
+import javax.inject.Inject
+
+@HiltViewModel
+class TransactionDetailViewModel @Inject constructor(
+    private val mUseCaseHandler: UseCaseHandler,
+    private val mFetchAccountTransferUseCase: FetchAccountTransfer
+) : ViewModel() {
+
+    private val _transactionDetailUiState: MutableStateFlow<TransactionDetailUiState> =
+        MutableStateFlow(TransactionDetailUiState.Loading)
+    val transactionDetailUiState get() = _transactionDetailUiState
+
+    fun getTransferDetail(transferId: Long) {
+        mUseCaseHandler.execute(mFetchAccountTransferUseCase,
+            FetchAccountTransfer.RequestValues(transferId),
+            object : UseCase.UseCaseCallback<FetchAccountTransfer.ResponseValue?> {
+                override fun onSuccess(response: FetchAccountTransfer.ResponseValue?) {
+                    _transactionDetailUiState.value =
+                        TransactionDetailUiState.Success(response?.transferDetail)
+                }
+
+                override fun onError(message: String) {
+                    _transactionDetailUiState.value = TransactionDetailUiState.Error
+                }
+            }
+        )
+    }
+}
+
+sealed class TransactionDetailUiState {
+    data object Loading : TransactionDetailUiState()
+    data object Error : TransactionDetailUiState()
+    data class Success(val transferDetail: TransferDetail?) : TransactionDetailUiState()
+}
