5036803c10f86d2a6d09b5989abc757859d8d1bc
Aditya <adityakumdale@gmail.com>
2024-06-20 12:35:44 +0530

    Move Merchants to feature module

---
+++ feature/merchants/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    alias(libs.plugins.mifospay.android.feature)
+    alias(libs.plugins.mifospay.android.library.compose)
+}
+
+android {
+    namespace = "org.mifospay.feature.merchants"
+}
+
+dependencies {
+    implementation(projects.core.data)
+    implementation(libs.compose.material)
+
+    //Todo: Remove these after migration of MerchantTransferActivity
+    implementation("com.jakewharton:butterknife-annotations:10.2.3")
+    implementation("com.jakewharton:butterknife:10.2.3@aar")
+    implementation("com.mifos.mobile:mifos-passcode:0.3.0@aar")
+    implementation(project(":mifospay"))
+}
