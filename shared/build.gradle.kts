09a7ecc097a6730827be0043d61538cf2e6aae9b
Pratyush Singh <90026952+PratyushSingh07@users.noreply.github.com>
2024-07-25 21:38:07 +0530

    feat: shared module setup (#1711)
    * feat: shared module setup
    
    * feat: ios xcode setup

---
+++ shared/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+        }
+    }
+}
+
+android {
+    namespace = "org.mifospay.shared"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
