7a265eaf9bd2cb98ddf0c096646a2c8733515323
Aditya <adityakumdale@gmail.com>
2024-08-26 21:47:03 +0530

    PreferencesHelper removed for datastore implementation

--- shared/src/androidMain/kotlin/org/mifospay/shared/di/Modules.android.kt
+++ shared/src/androidMain/kotlin/org/mifospay/shared/di/Modules.android.kt
@@ -1,9 +1,7 @@
 package org.mifospay.shared.di
 
-import android.content.Context
 import org.koin.dsl.module
-import org.mifospay.shared.core.data.PreferencesHelper
 
 actual val platformModule = module {
-    single { PreferencesHelper(get<Context>()) }
+
 }
--- shared/src/iosMain/kotlin/org/mifospay/shared/core/data/PreferencesHelper.ios.kt
+++
@@ -1,6 +0,0 @@
-package org.mifospay.shared.core.data
-
-import org.mifospay.shared.modal.domain.Client
-import org.mifospay.shared.modal.domain.User
-
-actual class PreferencesHelper
