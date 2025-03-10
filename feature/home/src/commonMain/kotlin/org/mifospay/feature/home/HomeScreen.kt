a4d521646482d9b94386738a74c989cb2be54c8f
rohitlokhande <lokhanderohit2020@gmail.com>
2025-03-10 13:41:37 +0530

    fix: Update color scheme in HomeScreen and TransactionHistoryCard components

--- feature/home/src/commonMain/kotlin/org/mifospay/feature/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/org/mifospay/feature/home/HomeScreen.kt
@@ -62,7 +62,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
