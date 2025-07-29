b18ff37d603bfaaf3ae67c2b3292cd1b3a32348b
Hekmatullah <hekmatullah.aminullah2@gmail.com>
2025-07-29 20:09:30 +0100

    chore: sync missing root files and folders from KMP project template

---
+++ core-base/database/build.gradle.kts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/mobile-wallet/blob/master/LICENSE.md
+ */
+import org.jetbrains.compose.compose
+
+/*
+ * Copyright 2025 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/kmp-project-template/blob/main/LICENSE
+ */
+plugins {
+    alias(libs.plugins.kmp.library.convention)
+}
+
+android {
+    namespace = "template.core.base.database"
+}
+
+kotlin {
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.androidx.room.runtime)
+        }
+
+        desktopMain.dependencies {
+            implementation(libs.androidx.room.runtime)
+        }
+
+        nativeMain.dependencies {
+            implementation(libs.androidx.room.runtime)
+        }
+
+        nonJsCommonMain.dependencies {
+            implementation(libs.androidx.room.runtime)
+        }
+    }
+}
