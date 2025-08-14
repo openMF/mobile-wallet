e8b7c945658368c7e28ae97b082fc89103fe8be6
Hekmatullah <hekmatullah.aminullah2@gmail.com>
2025-08-14 22:48:18 +0100

    misc: optimize slow cmp_shared build step in CI (#1904)

--- cmp-shared/build.gradle.kts
+++ cmp-shared/build.gradle.kts
@@ -9,27 +9,15 @@
  */
 
 plugins {
-    alias(libs.plugins.kmp.library.convention)
     alias(libs.plugins.cmp.feature.convention)
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.kotlinCocoapods)
 }
 
 kotlin {
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-            optimized = true
-        }
-    }
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
 
     sourceSets {
         commonMain.dependencies {
@@ -43,9 +31,6 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(compose.components.resources)
             implementation(libs.window.size)
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose)
-            implementation(libs.koin.compose.viewmodel)
 
             implementation(projects.feature.auth)
             implementation(projects.libs.mifosPasscode)
