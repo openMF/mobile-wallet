72505e58bead77aaf39c07aa24d4418692d98701
revanthkumarJ <jrevanth101@gmail.com>
2025-09-11 20:22:50 +0530

    initial fixes

--- feature/make-transfer/src/commonMain/kotlin/org/mifospay/feature/make/transfer/v2/MakeTransferScreenv2.kt
+++ feature/make-transfer/src/commonMain/kotlin/org/mifospay/feature/make/transfer/v2/MakeTransferScreenv2.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -235,6 +236,9 @@ internal fun MakeTransferScreenV2(
                             value = state.description,
                             isError = !state.descriptionIsValid,
                             onValueChange = { onAction(MakeTransferV2Action.DescriptionChanged(it)) },
+                            keyboardOptions = KeyboardOptions(
+                                imeAction = ImeAction.Done
+                            )
                         )
                     }
 
@@ -373,6 +377,7 @@ private fun EnterAmountCard(
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Next
                 ),
                 isError = !state.amountIsValid,
                 textStyle = KptTheme.typography.headlineMedium,
